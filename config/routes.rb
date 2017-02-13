54d6805fc9c9bd69409fa5cb6d2c8a2c7e2a24c8
staceymay <missstaceymay@gmail.com>
2017-02-13 15:41:13 -0800

    Updated Place update controlle2

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 
-   get "/", to: "home_pages#home"
+  get "/", to: "home_pages#home"
 
   get "/places", to: "places#index"
   get "/places/new", to: "places#new"
