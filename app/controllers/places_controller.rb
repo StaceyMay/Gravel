76086abcda7335c9474f2f29cc45d4257bab57cf
staceymay <missstaceymay@gmail.com>
2020-05-03 17:45:10 -0700

    update form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -28,8 +28,6 @@ before_action :authenticate_user!
   end
 
   def create
-    results = Geocoder.coordinates(params[:search])
-
     @place = Place.new(address: params[:search],phone: params[:phone], name: params[:name], trip_id: params[:trip_id] , latitude: results[0] , longitude: results[1])
     @place.save
 
